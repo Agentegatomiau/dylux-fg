ad1af992d195fd1ae307df39b2f55880e6e919e5
Agentegatomiau <99926462+Agentegatomiau@users.noreply.github.com>
2023-08-26 12:45:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['51940425018', 'Cat', true],
+  ['573016579583'], 
+  ['573016579583'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['51940425018'] 
+global.prems = ['51940425018', '573016579583', '573016579583']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -25,12 +25,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98' 
-global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
+global.packname = 'UwU' 
+global.author = 'Cat' 
+global.fgig = '▢ Canal de Youtube\nhttps://youtube.com/c/UniversoDelAnimeUDA\n' 
+global.dygp = 'https://chat.whatsapp.com/H2sKXp2y2OW7XdPOzwkZld'
 global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
+global.fgyt = 'https://youtube.com/c/UniversoDelAnimeUDA'
 global.fgpyp = 'https://paypal.me/fg98f'
 global.fglog = 'https://i.imgur.com/Owmb93c.png' 
 
